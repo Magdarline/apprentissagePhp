b19b97f9cedf3c55ea7129784b08b9ed565cd43d
Magdarline <magdarlined@gmail.com>
2021-09-02 12:42:39 +0200

    first commit

---
+++ exo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <title>Deuxieme projet</title>
+</head>
+<body>
+    <?php include("./partial/_navBar.php");?>
+    <h1>Les exercices</h1>
+    <ol>
+        <li>Exercice 1</li>
+    </ol>
+
+    <a href="">Ma repository github</a>
+
+    <script src="/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
